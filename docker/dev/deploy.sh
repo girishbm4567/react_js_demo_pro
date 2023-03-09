5d0a7c65c060c96e313baf9d7bf854ca6ef8e1a4
girishbm4567 <113753419+girishbm4567@users.noreply.github.com>
2023-03-10 00:40:58 +0530

    Update deploy.sh

--- docker/dev/deploy.sh
+++ docker/dev/deploy.sh
@@ -5,6 +5,6 @@ sudo docker-compose -f reactjs-dev-docker-compose.yml up -d
 
 sleep 10
 
-docker cp default.conf react_app:/etc/nginx/conf.d/
+sudo docker cp default.conf react_app:/etc/nginx/conf.d/
 
-docker exec react_app nginx -s reload
+sudo docker exec react_app nginx -s reload
